408028e5bdf93e107fa9c8368796b66a67b5302b
Marcos Alvarez <marcos.alvarez.91@gmail.com>
2020-10-24 12:30:56 -0300

    Added HelloApiView, HelloViewSet and serializers

---
+++ profiles_api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
+
+urlpatterns = [
+	path(r'hello-view/', views.HelloApiView.as_view()),
+	path(r'', include(router.urls)),
+]
